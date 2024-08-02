91fe70a365aeb9a9b63661d8f852e12e4cfe71bc
Cameron Boyle <cambboyle@gmail.com>
2024-08-02 16:51:42 +0000

    Add environ vars to settings.py

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -24,12 +24,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-@o_l+o(e44vw^j9ycdtnc6z86frpqhrj*5@ll2#f5b9xh0q!ul'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['cbb00-django-todo-app-609575cadd82.herokuapp.com']
+ALLOWED_HOSTS = [os.environ.get('HEROKU_HOSTNAME')]
 
 
 # Application definition
@@ -86,7 +86,7 @@ WSGI_APPLICATION = 'django_todo.wsgi.application'
 # }
 
 DATABASES = {
-    'default' : dj_database_url.parse(os.environ['DATABASE_URL'])
+    'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
 }
 
 
