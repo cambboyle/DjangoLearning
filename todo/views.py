f692b38ae6d5a3492efdeb280f1b448a9d923056
Cameron Boyle <cambboyle@gmail.com>
2024-08-02 16:23:14 +0000

    Fix error with Method Not Found error

--- todo/views.py
+++ todo/views.py
@@ -2,26 +2,27 @@ from django.shortcuts import render, redirect, get_object_or_404
 from .models import Item
 from .forms import ItemForm
 
+
 def get_todo_list(request):
     items = Item.objects.all()
-    context ={
+    context = {
         'items': items
     }
     return render(request, 'todo/todo_list.html', context)
+
 
 def add_item(request):
     if request.method == 'POST':
         form = ItemForm(request.POST)
         if form.is_valid():
             form.save()
-            return redirect('get_todo_list')  # Ensure redirect on successful form submission
-    else:
-        form = ItemForm()
-    
+            return redirect('get_todo_list')
+    form = ItemForm()
     context = {
         'form': form
     }
-    return render(request, 'todo/add_item.html', context)  # Render form for both GET and invalid POST scenarios
+    return render(request, 'todo/add_item.html', context)
+
 
 def edit_item(request, item_id):
     item = get_object_or_404(Item, id=item_id)
@@ -29,21 +30,20 @@ def edit_item(request, item_id):
         form = ItemForm(request.POST, instance=item)
         if form.is_valid():
             form.save()
-            return redirect('get_todo_list')  # Ensure redirect on successful form submission
-    else:
-        form = ItemForm(instance=item)
-    
+            return redirect('get_todo_list')
+    form = ItemForm(instance=item)
     context = {
-        'form': form,
-        'item': item
+        'form': form
     }
-    return render(request, 'todo/edit_item.html', context)  # Render form for both GET and invalid POST scenarios
+    return render(request, 'todo/edit_item.html', context)
+
 
 def toggle_item(request, item_id):
     item = get_object_or_404(Item, id=item_id)
     item.done = not item.done
     item.save()
     return redirect('get_todo_list')
+
 
 def delete_item(request, item_id):
     item = get_object_or_404(Item, id=item_id)
